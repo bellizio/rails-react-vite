c33298faa689465e1ee3c0004b24d44ff3f701c3
Devon Bellizio <devon.bellizio@vimeo.com>
2022-04-26 16:00:22 -0400

    add helppage redirect route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'homepage#index'
   get 'help', to: 'helppage#index'
+  # important for when there are react routes that extend beyond this route - without it, refreshing a page at a route such as /help/account-settings will throw a server error
+  get "help/*anything", to: "helppage#index"
   get 'browse', to: 'browsepage#index'
 
   namespace :api do
