16ff10336c46a83bf967005c3a6524780cb3a8f9
Devon Bellizio <devon.bellizio@vimeo.com>
2022-04-26 08:38:29 -0400

    add vite-plugin-full-reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import ruby from 'vite-plugin-ruby';
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import fullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   plugins: [
     ruby(),
-    react()
+    react(),
+    fullReload(['config/routes.rb', 'app/views/**/*']),
   ],
-})
+});
