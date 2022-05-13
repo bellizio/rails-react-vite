c526495462ac72e87361f78e9fede1626c847a69
Devon Bellizio <devon.bellizio@vimeo.com>
2022-05-13 08:14:58 -0400

    revert props change

--- app/frontend/components/Main/Main.tsx
+++ app/frontend/components/Main/Main.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { GreetingContainer, GreetingImage, GreetingText } from './styles';
 
-const Greeting = (props) => {
+type GreetingProps = {
+  greeting: string;
+  imgData: {
+    src: string;
+    alt: string;
+  };
+  children: React.ReactNode;
+};
+
+const Greeting = (props: GreetingProps) => {
   const { greeting, imgData, children } = props;
   const { src, alt } = imgData;
 
