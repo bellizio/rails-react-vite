dd7239fd46888f15004bf3871ea0c2935ecd4f5c
bellizio <jdbellizio@gmail.com>
2022-12-28 09:43:24 -0600

    switch to plugin-react-swc for hmr

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import react from '@vitejs/plugin-react';
+// https://vitejs.dev/blog/announcing-vite4.html#new-react-plugin-using-swc-during-development
+// import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import fullReload from 'vite-plugin-full-reload';
 import ruby from 'vite-plugin-ruby';
